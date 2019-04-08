0cc40dd6ce3668ddd6404678e7b61652b59f6ad4
Jason Lee <lijiansen.sjtu@gmail.com>
2019-04-08 19:23:51 +0800

    image classification net using CIFAR10 dataset

--- test_CIFAR10.py
+++ test_CIFAR10.py
@@ -1,8 +1,11 @@
 import torch
+import torch.nn as nn
 import torchvision
 import torchvision.transforms as ts
 import matplotlib.pyplot as plt
 import numpy as np
+import torch.optim as optim
+from cnn import Net
 
 
 # normalize PILImages to Tensors of range [-1, 1]
@@ -34,5 +37,61 @@ if __name__ == '__main__':
     dataiter = iter(trainloader)
     images, labels = dataiter.next()
     imshow(torchvision.utils.make_grid(images, padding=2))
-    print(' '.join('%s' % classes[labels[j]] for j in range(4)))
     print(' '.join(classes[labels[j]] for j in range(4)))
+
+    net = Net()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    net.to(device)
+
+    # training the network
+    for epoch in range(2):
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            # forward
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            # backward
+            loss.backward()
+            #optimize
+            optimizer.step()
+
+            # print
+            running_loss += loss.item()
+            if i % 2000 == 1999:
+                print(f'[{epoch+1}, {i+1}] loss: {running_loss / 2000}')
+                running_loss = 0.0
+    print('Finished training.')
+
+    # test the network
+    dataiter = iter(testloader)
+    images, labels = dataiter.next()
+    imshow(torchvision.utils.make_grid(images))
+    print('Ground Truth: ', ' '.join(classes[labels[j]] for j in range(4)))
+
+    outputs = net(images)
+    _, predicted = torch.max(outputs, 1)
+    print('Predicted: ', ' '.join(classes[predicted[j]] for j in range(4)))
+
+    # accuracy
+    correct = [0] * 10
+    total = [0] * 10
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            c = predicted == labels
+            for i in range(4):
+                label = labels[i]
+                total[label] += 1
+                correct[label] += c[i].item()
+
+    for i in range(10):
+        print(f'Accuracy of {classes[i]}: {100 * correct[i] / total[i]} %')
+
